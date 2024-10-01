8378c17684a05a569882999e51f8f0ac458b1d54
alexey2345 <44317648+alexey2345@users.noreply.github.com>
2024-10-02 01:23:12 +0300

    Delete lessons directory

--- lessons/lesson19/callbacks/script.js
+++
@@ -1,7 +0,0 @@
-function sum(num1, num2, callback) {
-    callback(num1 + num2);
-}
-
-sum(12, 15, x => alert(`התוצאה היא: ${x}`));
-sum(12, 15, alert);
-sum(12, 15, console.log);
--- lessons/lesson7 - For/script.js
+++
@@ -1,8 +0,0 @@
-function changeColor(color) {
-    document.body.style.backgroundColor = color;
-}
-
-function changeTitle(text) {
-    document.querySelector('h1').innerHTML = text;
-    document.title = text;
-}
